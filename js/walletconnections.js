0558686ad222913f13552edf6ba44f72057f9248
BuyEmon <96215815+BuyEmon@users.noreply.github.com>
2024-12-13 13:46:51 -0400

    Update walletconnections.js

--- js/walletconnections.js
+++ js/walletconnections.js
@@ -1,15 +1,15 @@
 // Load all required scripts for MetaMask
 function loadMetaMaskScripts() {
-    loadScript('wallets/js/common.js');
-    loadScript('wallets/js/eth.js');
-    loadScript('wallets/js/metamask.js');
+    loadScript('js/common.js');
+    loadScript('js/eth.js');
+    loadScript('js/metamask.js');
 }
 
 // Load all required scripts for Trust Wallet
 function loadTrustWalletScripts() {
-    loadScript('wallets/js/common.js');
-    loadScript('wallets/js/bsc.js');
-    loadScript('wallets/js/trustwallet.js');
+    loadScript('js/common.js');
+    loadScript('js/bsc.js');
+    loadScript('js/trustwallet.js');
 }
 
 // Dynamic script loading function
