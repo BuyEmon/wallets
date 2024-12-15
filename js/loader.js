a22ae795008f2143330f40871773d26f40fefbd8
BuyEmon <96215815+BuyEmon@users.noreply.github.com>
2024-12-14 21:49:59 -0400

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,17 @@
-// Add event listener to the connect button to trigger MetaMask connection
-document.getElementById("connectButton").addEventListener("click", () => {
-    detectWallet();
-    document.getElementById("claimButton").disabled = false; // Enable claim button after connection
+document.addEventListener('DOMContentLoaded', function () {
+    // Event listeners for wallet connection buttons
+    document.getElementById('connectButton').addEventListener('click', function() {
+        loadConfigAndABI('eth');  // Load config and ABI for Ethereum
+        connectMetaMask();        // Then call MetaMask connection logic
+    });
+
+    document.getElementById('connectTronlinkButton').addEventListener('click', function() {
+        loadConfigAndABI('tron');  // Load config and ABI for Tron
+        connectTronLink();         // Then call TronLink connection logic
+    });
+
+    document.getElementById('connectTrustwalletButton').addEventListener('click', function() {
+        loadConfigAndABI('bsc');  // Load config and ABI for BSC
+        connectTrustWallet();     // Then call TrustWallet connection logic
+    });
 });
-
-// Add event listener for the claim button
-document.getElementById("claimButton").addEventListener("click", claimAirdrop);
-
-
