39b2e33b49e7430dd09309a02d060b8fddf9e725
BuyEmon <96215815+BuyEmon@users.noreply.github.com>
2024-12-16 19:38:12 -0400

    Update metamask-deep.js

--- deeplinks/metamask-deep.js
+++ deeplinks/metamask-deep.js
@@ -1,18 +1,9 @@
-console.log('metamask-deeplink.js: Mobile Deep Link Loaded');
+// metamask-deeplink.js
 
-
-function redirectToMetaMask() {
-    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-    const isMetaMask = navigator.userAgent.includes("MetaMask");
-
-    if (isMobile && !isMetaMask) {
-        const deepLinkURL = "https://metamask.app.link/dapp/yourdomain.com/asset/index.html";
-        console.log("Redirecting to MetaMask:", deepLinkURL);
-        window.location.href = deepLinkURL;
-
-        setTimeout(() => {
-            alert("If MetaMask did not open, open it manually and visit the site.");
-        }, 3000);
-    }
+function openMetaMaskMobile() {
+    const url = 'https://metamask.app.link/dapp/<your-dapp-url>';
+    window.location.href = url;
 }
 
+// Call this function on mobile to open MetaMask
+openMetaMaskMobile();
